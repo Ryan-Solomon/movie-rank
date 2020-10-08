92c5b8594a15d30dc1d1676f74239e8141477eed
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-10-08 13:20:36 -0500

    debounces input

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const APIKEY = '?apikey=${APIKEY}&';
-
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get('http://ombdapi.com/', {
     params: {
       apikey: '9a416301',
-      s: 'avengers',
+      s: searchTerm,
     },
   });
 };
+
+const input = document.querySelector('input');
+
+let timeoutId;
+
+const onInput = (event) => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(fetchData(event.target.value), 1000);
+};
+
+input.addEventListener('input', onInput);
