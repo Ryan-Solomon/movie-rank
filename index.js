0c90ef901ccf2ddbdfb0949eece24ddb9b443325
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-10-10 13:08:26 -0500

    converts to rendering html via js

--- index.js
+++ index.js
@@ -43,11 +43,52 @@ const onInput = async (e) => {
     option.innerHTML = `
     <img src="${imgSrc}" />
     ${movie.Title}
-    
     `;
+
+    input.addEventListener('click', () => {
+      drowpdown.classList.remove('is-active');
+      input.value = movie.Title;
+      onMovieSelect(movie);
+    });
 
     resultsWrapper.appendChild(option);
   });
 };
 
 input.addEventListener('input', debounce(onInput));
+
+document.addEventListener('click', (e) => {
+  if (!root.contains(e.target)) {
+    dropdown.classList.remove('is-active');
+  }
+});
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get('http://ombdapi.com/', {
+    params: {
+      apikey: '9a416301',
+      i: movie.imbdID,
+    },
+    movieTemplate(response.data)
+}
+
+  
+const movieTemplate = (movieData) => {
+  return (`
+  <article class="media">
+    <figure class="media-left">
+      <p class="image">
+        <img src="${movieData.Poster}"
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class = "content">
+        <h1>${movieData.Title}</h1>
+        <h4>${movieData.Genre}</h4>
+        <p>${movieData.Plot}</p>
+      </div>
+    </div>
+  </article>
+  `
+  )}
+
